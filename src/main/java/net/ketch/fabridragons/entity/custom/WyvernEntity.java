8839c5b228ea5d24e2d15dd6d473fdce1e0e5e95
Julia <julia.ketcham@oit.edu>
2024-09-17 19:20:11 -0700

    Is now tameable, fixed function "method_48926"

--- src/main/java/net/ketch/fabridragons/entity/custom/WyvernEntity.java
+++ src/main/java/net/ketch/fabridragons/entity/custom/WyvernEntity.java
@@ -1,67 +1,43 @@
 package net.ketch.fabridragons.entity.custom;
 
 import net.ketch.fabridragons.entity.ModEntities;
-//import net.minecraft.entity.AnimationState;
-//import net.minecraft.entity.Entity;
-//import net.minecraft.entity.EntityPose;
-//import net.minecraft.entity.EntityType;
-//import net.minecraft.entity.ai.goal.*;
-//import net.minecraft.entity.attribute.DefaultAttributeContainer;
-//import net.minecraft.entity.attribute.EntityAttributes;
-//import net.minecraft.entity.mob.MobEntity;
-//import net.minecraft.entity.passive.AnimalEntity;
-//import net.minecraft.entity.passive.MerchantEntity;
-//import net.minecraft.entity.passive.PassiveEntity;
-//import net.minecraft.entity.player.PlayerEntity;
-//import net.minecraft.item.ItemStack;
-//import net.minecraft.item.Items;
-//import net.minecraft.recipe.Ingredient;
-//import net.minecraft.server.world.ServerWorld;
-//import net.minecraft.world.World;
-//import org.jetbrains.annotations.Nullable;
-//import software.bernie.geckolib.animatable.GeoEntity;
-//import software.bernie.geckolib.core.animatable.GeoAnimatable;
-//import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
-//import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
-//import software.bernie.geckolib.core.animation.AnimatableManager;
-//import software.bernie.geckolib.core.animation.Animation;
-//import software.bernie.geckolib.core.animation.AnimationController;
-//import software.bernie.geckolib.core.animation.RawAnimation;
-//import software.bernie.geckolib.core.object.PlayState;
-
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.entity.passive.ChickenEntity;
-import net.minecraft.entity.passive.MerchantEntity;
-import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.passive.*;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.recipe.Ingredient;
+import net.minecraft.scoreboard.Team;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.EntityView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.GeoAnimatable;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
-import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.*;
 import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
-public class WyvernEntity extends AnimalEntity implements GeoEntity {
+public class WyvernEntity extends TameableEntity implements GeoEntity {
     private AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
-    public WyvernEntity(EntityType<? extends AnimalEntity> entityType, World world) {
+    public WyvernEntity(EntityType<? extends TameableEntity> entityType, World world) {
         super(entityType, world);
     }
 
     public static DefaultAttributeContainer.Builder createWyvernAttributes(){
-        return MobEntity.createMobAttributes()
+        return TameableEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 50)
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.5f)
                 .add(EntityAttributes.GENERIC_ARMOR, 3.0f)
@@ -72,12 +48,11 @@ public class WyvernEntity extends AnimalEntity implements GeoEntity {
     @Override
     protected void initGoals(){
         this.goalSelector.add(0, new SwimGoal(this)); //HAS to be primary goal or entity will drown lol
-        this.goalSelector.add(1, new AnimalMateGoal(this, 1.150));
-        this.goalSelector.add(2, new TemptGoal(this,2.30D, Ingredient.ofItems(Items.ENDER_PEARL), false));
-        this.goalSelector.add(3, new FollowParentGoal(this, 1.150));
-        this.goalSelector.add(4, new WanderAroundFarGoal(this, 1.150));
-        //this.goalSelector.add(5, new LookAtEntityGoal(this, PlayerEntity.class, 4f));
-        //this.goalSelector.add(6, new LookAroundGoal(this));
+        this.goalSelector.add(1, new SitGoal(this));  //sits when tamed i think
+        this.goalSelector.add(2, new AnimalMateGoal(this, 1.150));
+        this.goalSelector.add(3, new TemptGoal(this,2.30D, Ingredient.ofItems(Items.ENDER_PEARL), false));
+        this.goalSelector.add(4, new FollowParentGoal(this, 1.150));
+        this.goalSelector.add(5, new WanderAroundFarGoal(this, 1.150));
 
         //racism is fun
         this.targetSelector.add(2, new ActiveTargetGoal<>(this, MerchantEntity.class, true));
@@ -101,6 +76,12 @@ public class WyvernEntity extends AnimalEntity implements GeoEntity {
             tAnimationState.getController().setAnimation(RawAnimation.begin().then("walk", Animation.LoopType.LOOP));
             return PlayState.CONTINUE;
         }
+
+        if (this.isSitting()){
+            tAnimationState.getController().setAnimation(RawAnimation.begin().then("sit", Animation.LoopType.LOOP));
+            return PlayState.CONTINUE;
+        }
+
         tAnimationState.getController().setAnimation(RawAnimation.begin().then("idle", Animation.LoopType.LOOP));
         return PlayState.CONTINUE;
     }
@@ -109,9 +90,110 @@ public class WyvernEntity extends AnimalEntity implements GeoEntity {
         return cache;
     }
 
-    // I THINK i can leave this because breeding item
+    // I THINK I can leave this because breeding item
     @Nullable
     public boolean isBreedingItem(ItemStack stack){
         return stack.isOf(Items.ENDER_PEARL);
+    }
+
+    /*TAMEABLE ENTITY */
+    private static final TrackedData<Boolean> SITTING =
+            DataTracker.registerData(WyvernEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
+
+    @Override
+    public ActionResult interactMob(PlayerEntity player, Hand hand) {
+        ItemStack itemstack = player.getStackInHand(hand);
+        Item item = itemstack.getItem();
+
+        Item itemForTaming = Items.RAW_GOLD; //dragons like gold
+
+        if (item == itemForTaming && !isTamed()) {
+            if (this.getWorld().isClient()) {
+                return ActionResult.CONSUME;
+            } else {
+                if (!player.getAbilities().creativeMode) {
+                    itemstack.decrement(1);
+                }
+
+                if (!this.getWorld().isClient()) {
+                    super.setOwner(player);
+                    this.navigation.recalculatePath();
+                    this.setTarget(null);
+                    this.getWorld().sendEntityStatus(this, (byte)7);  //spawn tame particles?
+                    setSit(true);
+                }
+
+                return ActionResult.SUCCESS;
+            }
+        }
+
+        if(isTamed() && !this.getWorld().isClient() && hand == Hand.MAIN_HAND) {
+            setSit(!isSitting());
+            return ActionResult.SUCCESS;
+        }
+
+        if (itemstack.getItem() == itemForTaming) {
+            return ActionResult.PASS;
+        }
+
+        return super.interactMob(player, hand);
+    }
+
+    public void setSit(boolean sitting) {
+        this.dataTracker.set(SITTING, sitting);
+        super.setSitting(sitting);
+    }
+
+    public boolean isSitting() {
+        return this.dataTracker.get(SITTING);
+    }
+
+    @Override
+    public void setTamed(boolean tamed) {
+        super.setTamed(tamed);
+        if (tamed) {
+            getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH).setBaseValue(60.0D);
+            getAttributeInstance(EntityAttributes.GENERIC_ATTACK_DAMAGE).setBaseValue(4D);
+            getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED).setBaseValue((double)0.5f);
+        } else {
+            getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH).setBaseValue(30.0D);
+            getAttributeInstance(EntityAttributes.GENERIC_ATTACK_DAMAGE).setBaseValue(2D);
+            getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED).setBaseValue((double)0.25f);
+        }
+    }
+
+    @Override
+    public void writeCustomDataToNbt(NbtCompound nbt) {
+        super.writeCustomDataToNbt(nbt);
+        nbt.putBoolean("isSitting", this.dataTracker.get(SITTING));
+    }
+
+    @Override
+    public void readCustomDataFromNbt(NbtCompound nbt) {
+        super.readCustomDataFromNbt(nbt);
+        this.dataTracker.set(SITTING, nbt.getBoolean("isSitting"));
+    }
+
+    //what is this for
+    @Override
+    public Team getScoreboardTeam() {
+        return super.getScoreboardTeam();
+    }
+
+    public boolean canBeLeashedBy(PlayerEntity player) {
+        return false;
+    }
+
+    @Override
+    protected void initDataTracker() {
+        super.initDataTracker();
+        this.dataTracker.startTracking(SITTING, false);
+    }
+
+
+    @Override
+    public EntityView method_48926() {
+        return super.getWorld(); //someone on github said to try this
+        //it worked, this is the stupidest thing i have ever seen
     }
 }
